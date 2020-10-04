79ded42cae6479b6874ebbe1f5e589151e465fd7
YoshiakiHirose <yoshiaki02231998@gmail.com>
2020-10-04 14:37:13 +0900

    Initial Commit

---
+++ Pods/Target Support Files/Pods-dig4-dig4Tests/Pods-dig4-dig4Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dig4_dig4Tests : NSObject
+@end
+@implementation PodsDummy_Pods_dig4_dig4Tests
+@end
---
+++ Pods/Target Support Files/Pods-dig4-dig4UITests/Pods-dig4-dig4UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dig4_dig4UITests : NSObject
+@end
+@implementation PodsDummy_Pods_dig4_dig4UITests
+@end
